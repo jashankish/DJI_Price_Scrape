d1ceef9664822c63330a97663969c2a0be6df0c4
Jashan Kishore <jkishore@umich.edu>
2018-09-12 22:04:52 -0400

    Create Analyzer.cpp

---
+++ Analyzer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <iomanip>
+#include <limits>
+#include <math.h>
+#include <queue>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Beginning to analyze the scraped data" << endl;
+    
+    ifstream dataInput;
+    dataInput.open("fileName.xlsx");
+    
+    //Paste algorithm from VS...
+    
+}
